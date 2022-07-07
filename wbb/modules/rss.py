868d6c04cc5975a13860c52181db47474f19b7c6
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-07-07 15:56:08 +0530

    Update rss.py

--- wbb/modules/rss.py
+++ wbb/modules/rss.py
@@ -55,9 +55,12 @@ async def rss_worker():
 
                 if feed.title == last_title:
                     continue
-
+                lmao = feed.link
+                #await app.send_message(
+                    #chat, feed.parsed(), disable_web_page_preview=True
+                #)
                 await app.send_message(
-                    chat, feed.parsed(), disable_web_page_preview=True
+                    chat, lmao, disable_web_page_preview=True
                 )
                 await update_rss_feed(chat, feed.title)
             except (
